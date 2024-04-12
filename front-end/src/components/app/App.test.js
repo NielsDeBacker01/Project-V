e9c3763cfd0249042b3eda66d710f2ef4510ccbf
NielsDeBacker01 <niels.debacker01@student.ap.be>
2024-04-12 13:25:37 +0200

    implement basic testing requirements

--- front-end/src/components/app/App.test.js
+++ front-end/src/components/app/App.test.js
@@ -1,8 +1,16 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the no graph selected text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/select a graph to display/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('tests can fail', () => {
+  render(<App />);
+  const linkElement = screen.queryByText(/gibberish fdlmvxcwjvci/i);
+  expect(linkElement).toBeNull();
 });
