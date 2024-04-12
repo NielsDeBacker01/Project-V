de9676246b3ddfe2271e24207faf1f5e213d8f7b
NielsDeBacker01 <niels.debacker01@student.ap.be>
2024-04-12 13:51:25 +0200

    finish app test

--- front-end/src/components/app/App.test.js
+++ front-end/src/components/app/App.test.js
@@ -5,8 +5,8 @@ import App from './App';
 
 test('renders the no graph selected text on start', () => {
   render(<App />);
-  const linkElement = screen.queryByText(/select a graph to display/i);
-  expect(linkElement).not.toBeNull();
+  const textElement = screen.queryByText(/select a graph to display/i);
+  expect(textElement).not.toBeNull();
 });
 
 test('renders sidebar', () => {
