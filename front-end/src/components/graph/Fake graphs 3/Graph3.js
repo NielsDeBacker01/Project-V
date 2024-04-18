112d2e015b1c8d71457f51cb1faf1c0cf8f3bf83
NielsDeBacker01 <niels.debacker01@student.ap.be>
2024-04-18 16:40:03 +0200

    fix button styling

---
+++ front-end/src/components/graph/Fake graphs 3/Graph3.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Sketch from 'react-p5';
+
+const Graph3 = () => {
+  let randomY = [];
+  let numPts = 100;
+
+  const setup = (p5, canvasParentRef) => {
+    //this is slightly too wide causing it to go off screen
+    p5.createCanvas(1200, 400).parent(canvasParentRef);
+    p5.background(255,204,0);
+    for (let i = 0; i < numPts; i++) {
+      randomY.push(p5.random(100, 300));
+    }
+  };
+
+  const draw = p5 => {
+    p5.strokeWeight(10);
+    p5.stroke(4, 214, 137);
+    drawLines(p5);
+
+    p5.strokeWeight(5);
+    p5.stroke(0, 183, 186);
+    drawLines(p5);
+  };
+
+  const drawLines = p5 => {
+    let px = 0;
+    let py = randomY[0];
+
+    for (let i = 0; i < randomY.length; i++) {
+      const x = i * (p5.width / (numPts - 1));
+      const y = randomY[i];
+
+      p5.line(px, py, x, y);
+
+      px = x;
+      py = y;
+    }
+  };
+
+  return <Sketch setup={setup} draw={draw} />;
+};
+
+Graph3.displayName = 'Graph 3';
+export default Graph3;
