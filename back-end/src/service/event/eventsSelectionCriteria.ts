fe05a2bf51c49da870181a8d8c0b7cea5c55d243
NielsDeBacker01 <niels.debacker01@student.ap.be>
2024-05-02 16:15:17 +0200

    make cs2 specific filters

--- back-end/src/service/event/eventsSelectionCriteria.ts
+++ back-end/src/service/event/eventsSelectionCriteria.ts
@@ -43,14 +43,12 @@ export class eventSelectionCriteria {
                timeout: ["id", "statePath", "type", "sequenceNumber", "teams", "segments", "draftActions"],
                team: [],
                player: [],
-               item: ["id","statePath"],
-               ability: ["id"],
                plantBomb: ["id","statePath","type"],
-               defuseBomb: ["id","statePath","type"],
-               beginDefuseBomb: ["id","statePath","type"],
-               reachDefuseBombCheckpoint: ["id","statePath","type"],
-               stopDefuseBomb: ["id","statePath","type"],
-               explodeBomb: ["id","statePath","type"],
+               explodeBomb: ["id","statePath","type"],               
+               beginDefuseWithoutKit: ["id","statePath","type"],
+               beginDefuseWithKit: ["id","statePath","type"],
+               map: ["id","statePath","type"],
+               tournament: ["id"],               
             }
       }
       //default filters that are the same for each game title
@@ -76,25 +74,29 @@ export class eventSelectionCriteria {
       timeRelatedFilters: [
          "round-started-freezetime", "round-ended-freezetime", "freezetime-started-timeout",
          "freezetime-ended-timeout", "game-set-clock", "game-started-clock",
-         "game-stopped-clock"
+         "game-stopped-clock", "series-paused-game", "series-resumed-game",
       ],
       bombRelatedFilters: [
          "player-completed-plantBomb", "team-completed-plantBomb", "team-completed-defuseBomb",
          "player-completed-defuseBomb", "player-completed-beginDefuseBomb", "player-completed-reachDefuseBombCheckpoint",
-         "player-completed-stopDefuseBomb", "player-completed-explodeBomb", "team-completed-explodeBomb"
+         "player-completed-stopDefuseBomb", "player-completed-explodeBomb", "team-completed-explodeBomb", 
+         "player-completed-beginDefuseWithKit", "player-completed-beginDefuseWithoutKit"
       ],
       combatRelatedFilters: [
          "player-revived-player", "player-selfrevived-player", "game-killed-player",
          "player-killed-player", "player-selfkilled-player", "player-teamkilled-player",
-         "player-pickedUp-item", "player-dropped-item", "player-used-ability"
+         "player-pickedUp-item", "player-dropped-item", "player-used-ability",
+         "player-damaged-player", "player-selfdamaged-player", "player-teamdamaged-player"
       ],
       gameStructureRelatedFilters: [
          "team-won-series", "team-won-game", "series-started-game",
          "series-ended-game", "team-won-round", "game-started-round",
          "game-ended-round"
+      ],
+      pregameRelatedFilter: [
+         "team-picked-map", "team-banned-map", "series-picked-map"
       ]
    };
-
 }
 
 export enum GameTitle {
