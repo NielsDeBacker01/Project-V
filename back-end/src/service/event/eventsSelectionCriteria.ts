e1b1514ab2be3c87c048e17b63aa7db7bf72bf1a
NielsDeBacker01 <niels.debacker01@student.ap.be>
2024-05-13 17:16:01 +0200

    edit eventSelectionCriteria

--- back-end/src/service/event/eventsSelectionCriteria.ts
+++ back-end/src/service/event/eventsSelectionCriteria.ts
@@ -1,103 +1,86 @@
 import { Filter, FilterNone } from "./filter";
 
 export class eventSelectionCriteria {
+   //used to filter out certain types of events
    bannedEventTypes: string[];
-   transactionFieldsToDelete: string[];
-   eventFieldsToDelete: string[];
-   actorTargetFieldsToDelete: object;
+   //applies certain criteria to the events
    criteriaFilterer: Filter;
+   //delete common values from transactions
+   transactionFieldsToDelete: string[];
+   //delete common values from events
+   eventFieldsToDelete: string[];
+   //used to filter the fields in the state/statedelta of an actor/target
+   actorTargetFieldsToDelete: object;
+   //adds exceptions to keep certain parts of seriesState(delta) which will normally be deleted entirely when this is empty
    seriesStateAndDeltaExceptions: string[]
    
    //provides default values that are usually what is needed
    constructor(game: GameTitle, filter: Filter = new FilterNone()) {
+      //default filters that are the same for each game title
+      this.criteriaFilterer = filter;
+      this.transactionFieldsToDelete = ["id", "correlationId", "seriesId"];
+      this.eventFieldsToDelete = ["id", "includesFullState", "seriesStateDelta", "seriesState"];
+      this.seriesStateAndDeltaExceptions = [];
+      this.bannedEventTypes = this.eventTypefilterTemplates.defaultFilters.concat(this.eventTypefilterTemplates.timeRelatedFilters);
+      
+      //default actorTargetFieldsToDelte for the current cases
+      //use an "_" to indicate filtering for a subfield of a actor/target field.
+      this.actorTargetFieldsToDelete = {
+         series: ["id","games","draftActions"],
+         series_teams: this.actorTargetFieldfilterTemplates.teamsFilters,
+         series_teams_players: this.actorTargetFieldfilterTemplates.teamsPlayersFilters,
+         game: ["statePath","structures","nonPlayerCharacters","segments","draftActions"],
+         game_teams: this.actorTargetFieldfilterTemplates.teamsFilters,
+         game_teams_players: this.actorTargetFieldfilterTemplates.teamsPlayersFilters,
+         game_clock: ["id", "type"],
+         round: ["statePath", "type", "sequenceNumber", "teams", "segments", "draftActions"],
+         clock: ["id", "type"],
+         freezetime: ["id", "statePath", "type", "sequenceNumber", "teams", "segments", "draftActions"],
+         timeout: ["id", "statePath", "type", "sequenceNumber", "teams", "segments", "draftActions"],
+         team_series: ["id"],
+         team_objectives: ["id", "statePath"],
+         team_game: ["id"],
+         team_game_series: ["id"],
+         team_game_series_objectives: ["id", "statePath"],
+         team_round: ["id"],
+         player_game: ["id","name","money"],
+         player_game_objectives: ["id", "statePath"],
+         player_series: ["id", "name", "participationStatus"],
+         player_series_objectives: ["id", "statePath"],
+         player_round: ["statePath"],
+         player_round_objectives: ["id", "statePath"],
+         item: ["id","statePath"],
+         ability: ["id"],
+         plantBomb: ["id", "statePath", "type"],
+         explodeBomb: ["id", "statePath", "type"]
+      };
+
       //filters with fields that might be game specific
       switch (game) {
          case GameTitle.VALORANT:
-            this.bannedEventTypes = this.eventTypefilterTemplates.defaultFilters.concat(this.eventTypefilterTemplates.timeRelatedFilters);
             //used to filter the fields in the state/statedelta of an actor/target
             //use an "_" to indicate filtering for a subfield of a actor/target field.
             this.actorTargetFieldsToDelete = {
-               series: ["id","games","draftActions"],
-               series_teams: this.actorTargetFieldfilterTemplates.teamsFilters,
-               series_teams_players: this.actorTargetFieldfilterTemplates.teamsPlayersFilters,
-               game: ["statePath","structures","nonPlayerCharacters","segments","draftActions"],
-               game_teams: this.actorTargetFieldfilterTemplates.teamsFilters,
-               game_teams_players: this.actorTargetFieldfilterTemplates.teamsPlayersFilters,
-               game_clock: ["id", "type"],           
-               round: ["statePath", "type", "sequenceNumber", "teams", "segments", "draftActions"],
-               clock: ["id", "type"],
-               freezetime: ["id", "statePath", "type", "sequenceNumber", "teams", "segments", "draftActions"],
-               timeout: ["id", "statePath", "type", "sequenceNumber", "teams", "segments", "draftActions"],
-               team_series: ["id"],
-               team_objectives: ["id", "statePath"],
-               team_game: ["id"],
-               team_game_series: ["id"],
-               team_game_series_objectives: ["id", "statePath"],
-               team_round: ["id"],
-               player_game: ["id","name","money"],
-               player_game_inventory_items: ["id", "statePath"],
+               ...this.actorTargetFieldsToDelete,
                player_game_abilities: ["id"],
-               player_game_objectives: ["id", "statePath"],
-               player_series: ["id", "name", "participationStatus"],
-               player_series_objectives: ["id", "statePath"],
-               player_round: ["statePath"],
-               player_round_objectives: ["id", "statePath"],
-               item: ["id","statePath"],
-               ability: ["id"],
-               plantBomb: ["id", "statePath", "type"],
-               defuseBomb: ["id", "statePath", "type"],
+               player_game_inventory_items: ["id", "statePath"],
                beginDefuseBomb: ["id", "statePath", "type"],
                reachDefuseBombCheckpoint: ["id", "statePath", "type"],
-               stopDefuseBomb: ["id", "statePath", "type"],
-               explodeBomb: ["id", "statePath", "type"],
-            }
+               stopDefuseBomb: ["id", "statePath", "type"]
+            };
             break;
          case GameTitle.CS2:
-            this.bannedEventTypes = this.eventTypefilterTemplates.defaultFilters.concat(this.eventTypefilterTemplates.timeRelatedFilters);
             //used to filter the fields in the state/statedelta of an actor/target
             //use an "_" to indicate filtering for a subfield of a actor/target field.
             this.actorTargetFieldsToDelete = {
-               series: ["id","games","draftActions"],
-               series_teams: this.actorTargetFieldfilterTemplates.teamsFilters,
-               series_teams_players: this.actorTargetFieldfilterTemplates.teamsPlayersFilters,
-               game: ["statePath","structures","nonPlayerCharacters","segments","draftActions"],
-               game_teams: this.actorTargetFieldfilterTemplates.teamsFilters,
-               game_teams_players: this.actorTargetFieldfilterTemplates.teamsPlayersFilters,
-               game_clock: ["id", "type"],    
-               round: ["statePath", "type", "sequenceNumber", "teams", "segments", "draftActions"],
-               clock: ["id", "type"],
-               freezetime: ["id", "statePath", "type", "sequenceNumber", "teams", "segments", "draftActions"],
-               timeout: ["id", "statePath", "type", "sequenceNumber", "teams", "segments", "draftActions"],
-               team_series: ["id"],
-               team_objectives: ["id", "statePath"],
-               team_game: ["id"],
-               team_game_series: ["id"],
-               team_game_series_objectives: ["id", "statePath"],
-               team_round: ["id"],
-               player_game: ["id","name","money"],
-               player_game_inventory_items: ["id", "statePath"],
-               player_game_abilities: ["id"],
-               player_game_objectives: ["id", "statePath"],
-               player_series: ["id", "name", "participationStatus"],
-               player_series_objectives: ["id", "statePath"],
-               player_round: ["statePath"],
-               player_round_objectives: ["id", "statePath"],
-               plantBomb: ["id","statePath","type"],
-               explodeBomb: ["id","statePath","type"],               
-               beginDefuseWithoutKit: ["id","statePath","type"],
-               beginDefuseWithKit: ["id","statePath","type"],
-               map: ["id","statePath","type"],
-               tournament: ["id"],               
-            }
+               ...this.actorTargetFieldsToDelete,
+               beginDefuseWithoutKit: ["id", "statePath", "type"],
+               beginDefuseWithKit: ["id", "statePath", "type"],
+               map: ["id", "statePath", "type"],
+               tournament: ["id"]
+            };
             break;
       }
-      //default filters that are the same for each game title
-      this.transactionFieldsToDelete = [
-         "id", "correlationId", "seriesId"];
-      this.eventFieldsToDelete = [
-         "id", "includesFullState", "seriesStateDelta", "seriesState"];
-      this.criteriaFilterer = filter;
-      this.seriesStateAndDeltaExceptions = [];
    }
 
    //Available filter sets for use in bannedEventTypes
